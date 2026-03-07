5bf24cc89cb5fcfbf2eb1ee900c5d2a1912ce590
Beckett Harland <bee@Becketts-MacBook-Air.local>
2026-03-07 19:58:26 +1300

    dictionary clean up, mainly making the pokemon a dictionary with the key being their name, rather than a list

--- pokemon/dictionary.py
+++ pokemon/dictionary.py
@@ -16,16 +16,19 @@ class Move:
     def use(self, user: Pokemon, target: Pokemon):
         print(f"{user.name} used {self.name}")
         if random.random() <= self.accuracy:
-            print(f"hit!\n"
-                  f"{target.name} took {self.damage} damage")
-            return self.damage
+            target.hp -= self.damage
+            print(f"hit!\n{target.name} took {self.damage} damage")
+
         else:
             print("Oh no! you missed!")
             return None
 
 
+# a class for any pokemon, with a name, level, and basic hp multiplier. Also has moveset, types and hp. Need to add attack, defence, and speed stats, and possibly even correct scaling logic based on level for stats
 class Pokemon:
-    def __init__(self, name: str, types: list, hp_mod: int, moveset: list, level: int = 1):
+    def __init__(
+        self, name: str, types: list, hp_mod: int, moveset: list, level: int = 1
+    ):
         self.name = name
         self.types = types
         self.level = level
@@ -34,12 +37,13 @@ class Pokemon:
         self.moveset = moveset
 
     def __repr__(self):
-        return f"Pokemon({self.name}, {self.types}, {self.hp_mod}, {self.hp}, {self.moveset}, {self.level})"
+        return f"Pokemon({self.name}, {self.types}, {self.max_hp}, {self.hp}, {self.moveset}, {self.level})"
 
     def __str__(self):
         return self.name
 
 
+# base item class to be overriden, has a use ability that targets a pokemon. Not much else yet
 class Item:
     def __init__(self, name: str):
         self.name = name
@@ -47,10 +51,11 @@ class Item:
     def __str__(self):
         return self.name
 
-    def use(self):  # gonna be over-ridden by subclasses
-        pass
+    def use(self, target: Pokemon):  # gonna be over-ridden by subclasses
+        raise NotImplementedError
 
 
+# class for healing items, heals a pokemons hp by it's strength. Might add some other stuff later (special effects etc)
 class Healing_Item(Item):
     def __init__(self, name, power):
         self.name = name
@@ -86,19 +91,34 @@ moves = [
     Move("ember", 40, 1, "fire")
 ]
 
-pokemon_data = [
-    ("zubat", ["flying", "dark"], 10, get_moves(moves, ["wing attack", "bite"])),
-    ("snorlax", ["normal"], 70, get_moves(moves, ["headbutt", "body slam"])),
-    ("pikachu", ["electric"], 20, get_moves(moves, ["thunder shock", "quick attack"])),
-    ("bulbasaur", ["grass", "poison"], 30, get_moves(moves, ["vine whip", "tackle"])),
-    ("squirtle", ["water"], 25, get_moves(moves, ["water gun", "tackle"])),
-    ("charmander", ["fire"], 20, get_moves(moves, ["ember", "tackle"]))
-]
+# dataset for moves. will add more when they are needed
+moves = {
+    "wing attack": Move("wing attack", 60, 1, "flying"),
+    "bite": Move("bite", 60, 1, "normal"),
+    "headbutt": Move("headbutt", 70, 1, "normal"),
+    "body slam": Move("body slam", 85, 1, "normal"),
+    "thunder shock": Move("thunder shock", 40, 1, "lightning"),
+    "quick attack": Move("quick attack", 40, 1, "normal"),
+    "vine whip": Move("vine whip", 45, 1, "grass"),
+    "tackle": Move("tackle", 40, 1, "normal"),
+    "water gun": Move("water gun", 40, 1, "water"),
+    "scratch": Move("scratch", 40, 1, "normal"),
+    "ember": Move("ember", 40, 1, "fire"),
+}
 
-healing_item_data = [
-    ("potion", 20)
-]
+# dataset for pokemon. Need to add a lot more
+pokemon_data = {
+    "zubat": ("zubat", ["flying", "dark"], 10, [moves["wing attack"], moves["bite"]]),
+    "snorlax": ("snorlax", ["normal"], 70, [moves["headbutt"], moves["body slam"]]),
+    "pikachu": ("pikachu", ["electric"], 20, [moves["thunder shock"], moves["quick attack"]]),
+    "bulbasaur": ("bulbasaur", ["grass", "poison"], 30, [moves["vine whip"], moves["tackle"]]),
+    "squirtle": ("squirtle", ["water"], 25, [moves["water gun"], moves["tackle"]]),
+    "charmander": ("charmander", ["fire"], 20, [moves["ember"], moves["tackle"]]),
+}
+
+# dataset for healing items. Will add more later
+healing_item_data = [("potion", 20)]
 
 
-def create_pokemon(index):
-    return Pokemon(pokemon_data[index][0], pokemon_data[index][1], pokemon_data[index][2], pokemon_data[index][3])
+def create_pokemon(name):
+    return Pokemon(*pokemon_data[name])
